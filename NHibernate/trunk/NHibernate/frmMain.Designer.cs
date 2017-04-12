df0d4274b563bc3c175860ad35c0e9af62e19821
grnik <gr_nik@mail.ru>
2017-04-12 16:06:56 +0300

    Add additional

--- NHibernate/trunk/NHibernate/frmMain.Designer.cs
+++ NHibernate/trunk/NHibernate/frmMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace NHibernate
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btCreate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btCreate
+            // 
+            this.btCreate.Location = new System.Drawing.Point(13, 13);
+            this.btCreate.Name = "btCreate";
+            this.btCreate.Size = new System.Drawing.Size(75, 23);
+            this.btCreate.TabIndex = 0;
+            this.btCreate.Text = "Create";
+            this.btCreate.UseVisualStyleBackColor = true;
+            this.btCreate.Click += new System.EventHandler(this.btCreate_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.btCreate);
+            this.Name = "frmMain";
+            this.Text = "NHibernate";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btCreate;
     }
 }
 
--- NHibernate/trunk/NHibernate/frmMain.cs
+++ NHibernate/trunk/NHibernate/frmMain.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace NHibernate
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void btCreate_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
