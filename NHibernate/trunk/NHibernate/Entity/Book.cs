bd9cd8d466635bce5f7568e17b66f315ee68db07
grnik <gr_nik@mail.ru>
2017-04-12 16:09:49 +0300

    Add entity Book

---
+++ NHibernate/trunk/NHibernate/Entity/Book.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHibernate.Entity
+{
+
+    public class Book
+    {
+        private long id;
+        private string isbn;
+        private string title;
+
+        public virtual long ID
+        {
+            get { return id; }
+            protected set { id = value; }
+        }
+
+        public virtual string ISBN
+        {
+            get { return isbn; }
+            set { isbn = value; }
+        }
+
+        public virtual string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public Book()
+        {
+        }
+
+        public Book(string bookIsbn, string bookTitle)
+        {
+            isbn = bookIsbn;
+            title = bookTitle;
+        }
+    }
+
+}
